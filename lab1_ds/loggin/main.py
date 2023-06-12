1b98d45cb7aa05a65dd7baf67699d44a565904df
SkydanD <84770746+SkydanD@users.noreply.github.com>
2023-06-12 16:51:50 +0300

    Add files via upload

---
+++ lab1_ds/loggin/main.py
@@ -0,0 +1,55 @@
+from aiohttp import web
+from os import environ
+import json
+from global_utils import HEADERS
+from base_client import MapHz, GetInfoService
+from asyncio import get_event_loop
+
+loop = get_event_loop()
+loop.run_until_complete(GetInfoService.get_all_services())
+
+app = web.Application()
+instance_map = MapHz(
+    list_nodes=GetInfoService.nodes, 
+    cluster_name="dev"
+)
+instance_map.create_map("map_custom")
+
+
+async def get_massage(request):
+    return web.Response(body=json.dumps(
+            {
+                'Response': instance_map.map_dist.values().result() if instance_map.map_dist.size().result() else "Not Found Data"
+                }
+            ).encode('utf-8'), 
+        headers=HEADERS
+        )
+
+
+async def create_message(request):
+    data = await request.json()
+    instance_map.send_data(unique_id=data.get('uuid'), massange=data.get('message'))
+    print(f"{data.get('uuid')} = {data.get('message')}")
+    return web.Response(body=json.dumps(
+            {
+                'Response': data
+                }
+            ).encode('utf-8'), 
+        headers=HEADERS
+        )
+    
+
+
+
+app.add_routes([
+    web.get('/', get_massage),
+    web.post('/', create_message)
+])
+
+
+if __name__ == "__main__":
+    web.run_app(
+        host="0.0.0.0",
+        port=environ.get("PORT_SEVICE"),
+        app=app
+        )
